6ae8691505e4f87ca0beafb5d1a3df024c4c4670
PayalKumari10 <kumaripayal7488@gmail.com>
2024-07-11 23:13:06 +0530

    create a card

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
 export const App = () => {
-  return <h1>Hello Payal, react-19</h1>
+  return (
+    <div>
+      <div style={{ textAlign: 'center' }}>
+        <img src="pic.jpg" alt="pic.jpg" width="30%" height="30%"/>
+      </div>
+      <h2>Name: Queen of Tears </h2>
+      <h3>Rating:8.2</h3>
+      <p>Summary: Baek Hyun-woo  both at home and at work. 
+        But upon deciding its future, visit to his family.
+      </p>
+    </div>
+  )
 }
+
+
+// React.createElement("h1", null, "Hello, Payal, react-19");
