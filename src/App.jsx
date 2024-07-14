616d2db4a606055852fbdbaf716117d71f354de1
PayalKumari10 <kumaripayal7488@gmail.com>
2024-07-15 02:37:21 +0530

    Add images, create series file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
-// import { Fragment } from "react";
-import  NetflixSeries, { Header,Footer } from "./components/NetflixSeries";
+import  NetflixSeries from "./components/NetflixSeries";
+
 export const App = () => {
   
-  // ********************   1    ***************
-  //  return ( [    <NetflixSeries key="1"/>, <NetflixSeries key="2"/>
-  //  ])
-
-    // ********************   2    ***************
-
-  // return (
-  //   <Fragment>
-  //   <NetflixSeries/>
-  //   <NetflixSeries/>
-  //   <NetflixSeries/>
-  //   <NetflixSeries/>
-  //   <NetflixSeries/>
-  //   </Fragment>
-  // );
-
-     // ********************   3    ***************
-
-
   return (
     <>
-    <Header/>
     <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <NetflixSeries/>
-    <Footer/>
     </>
   );
 };
--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,69 +1,33 @@
+import seriesData from "../api/seriesData.json"
 const NetflixSeries = () => {
-    const name = "Queen of Tears";
-    const rating = "8.2";
-    const summary = " Baek Hyun-woo  both at home and at work.But upon deciding its future, visit to his family."
-    let age = 19;
-    
-   //  let canWatch = "Not Available";
-   //  if(age >= 18) canWatch = "Watch Now";
-    
-
-    const canWatch = () => {
-     if (age >= 18) return "Watch Now";
-     return "Not Available";
-    };
-
-
-    const returnGenre = () => {
-     const genre = "RomCop";
-     return genre;
-    };
-
-
-   //  if(age < 18) {
-   //   return (
-   //     <div>
-   //       <div style={{ textAlign: 'center' }}>
-   //         <img src="pic.jpg" alt="pic.jpg" width="30%" height="30%"/>
-   //         <img src="pic1.jpg" alt="pic.jpg" width="30%" height="30%"/>
-   //       </div>
-   //       <h2>Name: {name} </h2>
-   //       <h3>Rating: {rating}</h3>
-   //       <p>Summary: {summary}</p>
-   //       <p>Genre: {returnGenre()}</p>
-   //       <button>Not Available</button>
-   //     </div>
-   //   );
-   //  }
-
-        
-   //2: But this violates DRY (Do not repeat yourself)
-
 
  return (
-   <div>
-     <div style={{ textAlign: 'center' }}>
-       <img src="pic.jpg" alt="pic.jpg" width="30%" height="30%"/>
-       <img src="pic1.jpg" alt="pic.jpg" width="30%" height="30%"/>
+  <ul>
+     {seriesData.map((curElem) => {
+      return (
+      <li key={curElem.id}>
+     <div>
+       <img src={curElem.img_url} 
+         alt={curElem.name}
+         width="40%"
+         height="40%"/>
      </div>
-     <h2>Name: {name} </h2>
-     <h3>Rating: {rating}</h3>
-     <p>Summary: {summary}</p>
-     <p>Genre: {returnGenre()}</p>
-     {/* <button>{age >= 18 ? "Watch Now" : "Not Available"}</button> */}
-      {/* <button>{canWatch()}</button> */}
-       <button>{canWatch()}</button>
+     <h2>Name: {curElem.name} </h2>
+     <h3>Rating: {curElem.rating}</h3>
+     <p>Summary: {curElem.summary}</p>
+     <p>Genre: {curElem.genre}</p>
+     <p>Cast: {curElem.cast}</p>
+       <a href={curElem.watch_url} target="_blank">
+       <button>Watch Now</button>
+       </a>
+   </li>
+      );
+      })}
 
-   </div>
+   </ul>
  );
 };
 
 export default NetflixSeries;
 
- export const Header = () => {
-    return <p>copyright @PayalKumar</p>;
-};
 
-export const Footer = () => {
-    return <p>copyright @PayalKumar</p>;
-};
