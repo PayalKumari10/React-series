b7b5a57a6d68c1551b21427c56a85ae7c6be032d
PayalKumari10 <kumaripayal7488@gmail.com>
2024-07-21 03:16:37 +0530

    Inline CSS

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,19 +1,31 @@
 /* eslint-disable react/prop-types */
 export const SeriesCard = ({ data }) => {
     const { img_url, name, rating, description, cast, genre, watch_url } = data;
+    
+    const btn_style = {
+      padding: "1.2rem 2.4rem",
+      border: "none",
+      fontSize: "1.6rem",
+      backgroundColor: "var(--bnt-hover-bg-color)",
+      color:"var(--bg-color)"
+    }
+
+
     return (
-      <li>
+      <li className="card">
         <div>
           <img src={img_url} alt={name} width="40%" height="40%" />
         </div>
+        <div className="card-content">
         <h2>Name: {name}</h2>
         <h3>Rating: {rating}</h3>
         <p>Summary: {description}</p>
         <p>Genre: {genre}</p>
         <p>Cast: {cast}</p>
         <a href={watch_url} target="_blank">
-          <button>Watch Now</button>
+          <button style={btn_style}>Watch Now</button>
         </a>
+        </div>
       </li>
     );
   };
