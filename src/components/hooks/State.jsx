96309ac6bd43eea0f748cab6328d7bb9499c979e
PayalKumari10 <kumaripayal7488@gmail.com>
2024-08-03 03:51:34 +0530

    React States

--- src/components/hooks/State.jsx
+++ src/components/hooks/State.jsx
@@ -1,15 +1,25 @@
-export const State = () => {
-  let value = 0;
-  const handleButtonClcik = () => {
-    value++;
-    console.log(value);
-  };
+import { useState } from 'react';
 
+export const State = () => {
+  // let value = 0;
+  // const handleButtonClcik = () => {
+  //   value++;
+  //   console.log(value);
+  // };
+
+  // let array = useState();
+    // console.log(array);
+
+  const [count, setCount] = useState(0);
+  const handleButtonClick = () => {
+    setCount(() => count + 1);
+  };
   return (
     <>
-    <section className="main-div"></section>
-    <h1>{value}</h1>
-    <button onClick={handleButtonClcik}>Increment</button>
+    <section className="main-div">
+    <h1>{count}</h1>
+    <button onClick={handleButtonClick}>Increment</button>
+    </section>
     </>
   );
 };
