67630ff208aca0a2e3582eb95b6b434b1bbeaabb
PayalKumari10 <kumaripayal7488@gmail.com>
2024-08-04 23:34:41 +0530

    react state works

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,17 @@ export const App = () => {
       {/* <EventProps/> */}
       {/* <EventPropagation/> */}
       <State/>
+      <Sibling/>
     </section>
   );
 };
+
+
+export function Sibling(){
+  console.log("Sibling Component rendered");
+  return (
+  <div className='main-div'>
+    <h2>Sibling Component</h2>
+    </div>
+  );
+}
--- src/components/hooks/State.jsx
+++ src/components/hooks/State.jsx
@@ -1,25 +1,39 @@
-import { useState } from 'react';
+import { useState } from "react";
+import PropTypes from "prop-types";
 
 export const State = () => {
-  // let value = 0;
-  // const handleButtonClcik = () => {
-  //   value++;
-  //   console.log(value);
-  // };
-
-  // let array = useState();
-    // console.log(array);
-
+  //   let value = 0;
+  //   const handleButtonClick = () => {
+  //     value++;
+  //     console.log(value);
+  //   };
+  //   let array = useState();
+  //   console.log(array);
   const [count, setCount] = useState(0);
+  console.log("Parent Component rendered");
   const handleButtonClick = () => {
     setCount(() => count + 1);
   };
   return (
     <>
-    <section className="main-div">
-    <h1>{count}</h1>
-    <button onClick={handleButtonClick}>Increment</button>
-    </section>
+      <div className="main-div">
+        <h1>{count}</h1>
+        <button onClick={handleButtonClick}>Increment</button>
+      </div>
+      <ChildComponent count={count} />
     </>
   );
+};
+
+function ChildComponent({ count }) {
+  console.log("Child Component rendered");
+  return (
+    <div className="main-div">
+      <h2> Child Component - {count} </h2>
+    </div>
+  );
+}
+
+ChildComponent.propTypes = {
+  count: PropTypes.number.isRequired,
 };
